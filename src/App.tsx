91fb48f240d8dba4291311150bab2da61680759f
BBIL-Team <deepshika5686@bharatbiotech.com>
2024-11-04 10:23:16 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { AmplifyProvider, Authenticator } from '@aws-amplify/ui-react';
+import { Authenticator, AuthenticatorProps } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 const App: React.FC = () => {
   return (
-    <AmplifyProvider>
-      <Authenticator components={{ SignUp: { hideSignUp: true } }}>
-        {({ signOut, user }) => (
-          <div>
-            <h1>Welcome, {user?.username}</h1>
-            <button onClick={signOut}>Sign out</button>
-          </div>
-        )}
-      </Authenticator>
-    </AmplifyProvider>
+    <Authenticator signUpAttributes={{ hideSignUp: true }}>
+      {({ signOut, user }) => (
+        <div>
+          <h1>Welcome, {user?.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </div>
+      )}
+    </Authenticator>
   );
 };
 
