ee4ce947311869343c22e7317f90b9abc5f2e8ae
BBIL-Team <deepshika5686@bharatbiotech.com>
2024-11-06 16:39:11 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ const App: React.FC = () => {
     try {
       const response = await fetch(' https://qvls5frwcc.execute-api.ap-south-1.amazonaws.com/V1/UploadLink_Anamay', {
         method: 'POST',
+        body: formData,
       });
 
       if (response.ok) {
